20a82e047575b2d43f4eb421549860f06e136abc
Aman Gupta <amang5840@gmail.com>
2021-05-12 16:36:13 +0530

    config passport-jwt

---
+++ config/passport-jwt-strategy.js
@@ -0,0 +1,25 @@
+const passport = require('passport');
+const JwtStrategy = require('passport-jwt').Strategy;
+const    ExtractJwt = require('passport-jwt').ExtractJwt;
+const User  = require('../models/user');
+
+const opts = {
+    jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey = 'secret',
+};
+
+passport.use(new JwtStrategy(opts, async (jwtPayload, done) => {
+    try {
+        const user = User.findById(jwtPayload._id);
+        if(user)
+        return done(null, user);
+
+        return done(null, false);
+    } catch (error) {
+        console.log("Error: ", error);
+        return res.status(500).json({
+            message: "Internal Server Error",
+            error
+        });
+    }
+}));
