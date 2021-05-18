a6d72ddaef4f621360f9afd207e38d7452530a32
Aman Gupta <amang5840@gmail.com>
2021-05-18 16:14:00 +0530

    upvote an article

--- controllers/api/v1/articles_controller.js
+++ controllers/api/v1/articles_controller.js
@@ -22,3 +22,43 @@ module.exports.create = async (req, res) => {
         });
     }
 };
+module.exports.toggleUpvote = async (req, res) => {
+    try {
+        const article = await Article.findById(req.params.id);
+
+        if (!article)
+            return res.status(404).json({
+                message: "Article not found",
+            });
+        const user = req.user;
+
+        const isDownVoted = article.downvotes.indexOf(user._id) > -1 ? true : false;
+        if(isDownVoted){
+            article.downvotes.pull(user._id);
+        }
+        const isUpvoted = article.upvotes.indexOf(user._id) > -1 ? true : false;
+
+        if (!isUpvoted) {
+            article.upvotes.push(user._id);
+        }
+        else {
+            article.upvotes.pull(user._id);
+        }
+        await article.save();
+
+        return res.status(200).json({
+            message: isUpvoted ? "Unupvoted Article" : "Upvoted Article",
+            data: {
+                article,
+                upvoted: !isUpvoted
+            }
+        });
+
+    } catch (error) {
+        console.log("Error: ", error);
+        return res.status(500).json({
+            message: "Internal Server Error",
+            error
+        });
+    }
+}
--- routes/api/v1/articles.js
+++ routes/api/v1/articles.js
@@ -5,5 +5,7 @@ const router = express.Router();
 const articleController = require('../../../controllers/api/v1/articles_controller');
 
 router.post('/create', passport.authenticate('jwt', { session: false }), articleController.create);
+router.post('/:id/upvote', passport.authenticate('jwt', { session: false }), articleController.toggleUpvote);
+// router.post('/:id/downvote', passport.authenticate('jwt', { session: false }), postController.toggleDownvote);
 
 module.exports = router;
