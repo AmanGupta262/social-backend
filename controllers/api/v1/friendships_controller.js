09a91ef0bc8782b1198d6f5e45083a21441b8efc
Aman Gupta <amang5840@gmail.com>
2021-05-22 00:23:42 +0530

    send friend request

--- controllers/api/v1/friendships_controller.js
+++ controllers/api/v1/friendships_controller.js
@@ -3,7 +3,32 @@ const Friendship = require('../../../models/friendship');
 
 module.exports.add = async (req, res) => {
     try {
+        const fromUser = req.user;
+        const toUser = await User.findOne({_id: req.params.id});
         
+        if(!toUser)
+            return res.status(404).json({
+                message: 'User not found'
+            })
+
+        const newFriendship = await Friendship.create({
+            to_user: toUser._id,
+            from_user: fromUser._id
+        });
+
+        toUser.friends.push(newFriendship);
+        fromUser.friends.push(newFriendship);
+
+        await toUser.save();
+        await fromUser.save();
+
+        return res.status(200).json({
+            message: "Request sent",
+            data: {
+                friendship: newFriendship
+            }
+        });
+
     } catch (error) {
         console.log("Error: ", error);
         return res.status(500).json({
--- routes/api/v1/friendships.js
+++ routes/api/v1/friendships.js
@@ -4,6 +4,6 @@ const passport = require('passport');
 
 const friendsController = require('../../../controllers/api/v1/friendships_controller');
 
-router.post('/add', passport.authenticate('jwt', {session: false}), friendsController.add);
+router.post('/:id/add', passport.authenticate('jwt', {session: false}), friendsController.add);
 
 module.exports = router;
