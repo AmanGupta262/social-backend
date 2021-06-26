1650ecdb68adcf75aa592e84d526f8117f982f6b
Aman Gupta <amang5840@gmail.com>
2021-06-26 20:37:03 +0530

    use mongodb atlas

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost/social", {
+mongoose.connect(process.env.MONGO_DB, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
